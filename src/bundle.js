32ffe82c6d6e23b9a9073d5c39f9aa9e4cecdb11
tksmrkm <nis.murakami@gmail.com>
2017-08-16 15:37:04 +0900

    store data saves into localstorage

--- src/bundle.js
+++ src/bundle.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { compose, createStore } from 'redux';
 import { render } from 'react-dom';
 import App from './App';
 import reducer from './reducers';
+import persistState from 'redux-localstorage'
 
-const initState = {
-    tabs: 0,
-    users: [],
-    dealings: []
-};
+const enhancer = compose(persistState(null, {key: 'payoff'}));
 
-let store = createStore(reducer, initState);
+let store = createStore(reducer, enhancer);
 
 render(
     <Provider store={store}>
