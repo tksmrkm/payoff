49e8cb98874e97397904b473a2696bbc2c0953c9
tksmrkm <nis.murakami@gmail.com>
2018-01-09 17:03:40 +0900

    building to dist/ directory as gh-pages

--- config/product.webpack.config.js
+++ config/product.webpack.config.js
@@ -1,9 +1,16 @@
 const webpack = require('webpack');
 const config = require('./common.webpack.config');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 config.plugins.push(
     new webpack.DefinePlugin({
         'process.env.NODE_ENV': JSON.stringify('production')
+    })
+);
+config.plugins.push(
+    new HtmlWebpackPlugin({
+        title: 'PayOff',
+        template: 'templates/index.html'
     })
 );
 
